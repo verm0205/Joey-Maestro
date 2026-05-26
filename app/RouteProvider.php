1b637e8a98897248499d0f39781f20f6a7509182
verm0205 <verm0205@hz.nl>
2026-05-26 11:59:57 +0200

    deleted white line

--- app/RouteProvider.php
+++ app/RouteProvider.php
@@ -25,6 +25,5 @@ class RouteProvider implements RouteProviderInterface
         // Dynamische blogpagina's
         // Deze route vangt alles op zoals /blog/swot, /blog/studiekeuze, etc.
         // $router->addRoute('GET', '/blog/(?<slug>[a-zA-Z0-9_-]+)', [$homeController, "blogShow"]);
-
     }
 }
