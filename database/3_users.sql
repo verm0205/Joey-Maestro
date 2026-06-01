92afe3e6944e1cee8f7f1ed965c54d5abeb8590e
joske vermeulen <jvermeulen5@student.scalda.nl>
2026-06-02 00:15:40 +0200

    text to varchat

--- database/3_users.sql
+++ database/3_users.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
    id       INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-   name     TEXT    NOT NULL,
-   username TEXT    NOT NULL UNIQUE,
-   password TEXT    NOT NULL,
-   role     TEXT    NOT NULL DEFAULT 'user' CHECK(role IN ('user','admin'))
+   name     VARCHAR     NOT NULL,
+   username VARCHAR     NOT NULL UNIQUE,
+   password VARCHAR     NOT NULL,
+   role     VARCHAR     NOT NULL DEFAULT 'user' CHECK(role IN ('user','admin'))
 );
 
 INSERT INTO users (name, username, password, role) VALUES (
