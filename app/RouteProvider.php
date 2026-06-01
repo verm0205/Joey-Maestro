98d7b159baf11c2cc82b7a3c939f19946b420899
joske vermeulen <jvermeulen5@student.scalda.nl>
2026-06-01 22:31:51 +0200

    phpcbf

--- app/RouteProvider.php
+++ app/RouteProvider.php
@@ -24,27 +24,27 @@ class RouteProvider implements RouteProviderInterface
         $router->addRoute('GET', '/tasks-api', fn($r) => $homeController->tasksApi($r));
 
         $gradeController = $container->get(GradeController::class);
-        $router->addRoute('GET','/dashboard',fn($r) => $gradeController->index($r));
-        $router->addRoute('GET','/grades/create',fn($r) => $gradeController->create($r));
-        $router->addRoute('POST','/grades',fn($r) => $gradeController->store($r));
-        $router->addRoute('GET','/grades/(?<id>[0-9]+)/edit',fn($r) => $gradeController->edit($r));
-        $router->addRoute('POST','/grades/(?<id>[0-9]+)/edit',fn($r) => $gradeController->update($r));
-        $router->addRoute('GET','/grades/(?<id>[0-9]+)/delete',fn($r) => $gradeController->deleteConfirm($r));
-        $router->addRoute('POST','/grades/(?<id>[0-9]+)/delete',fn($r) => $gradeController->delete($r));
+        $router->addRoute('GET', '/dashboard', fn($r) => $gradeController->index($r));
+        $router->addRoute('GET', '/grades/create', fn($r) => $gradeController->create($r));
+        $router->addRoute('POST', '/grades', fn($r) => $gradeController->store($r));
+        $router->addRoute('GET', '/grades/(?<id>[0-9]+)/edit', fn($r) => $gradeController->edit($r));
+        $router->addRoute('POST', '/grades/(?<id>[0-9]+)/edit', fn($r) => $gradeController->update($r));
+        $router->addRoute('GET', '/grades/(?<id>[0-9]+)/delete', fn($r) => $gradeController->deleteConfirm($r));
+        $router->addRoute('POST', '/grades/(?<id>[0-9]+)/delete', fn($r) => $gradeController->delete($r));
 
         $blogController = $container->get(BlogController::class);
-        $router->addRoute('GET','/blog',fn($r) => $blogController->index($r));
-        $router->addRoute('GET','/blog/manage',fn($r) => $blogController->manage($r));
-        $router->addRoute('GET','/blog/create',fn($r) => $blogController->create($r));
-        $router->addRoute('POST','/blog',fn($r) => $blogController->store($r));
-        $router->addRoute('GET','/blog/(?<id>[0-9]+)/edit', fn($r) => $blogController->edit($r));
-        $router->addRoute('POST','/blog/(?<id>[0-9]+)/edit',fn($r) => $blogController->update($r));
-        $router->addRoute('GET','/blog/(?<id>[0-9]+)/delete',fn($r) => $blogController->deleteConfirm($r));
-        $router->addRoute('POST','/blog/(?<id>[0-9]+)/delete',fn($r) => $blogController->delete($r));
-        $router->addRoute('GET','/blog/(?<path>[a-z0-9-]+)',fn($r) => $blogController->show($r));
+        $router->addRoute('GET', '/blog', fn($r) => $blogController->index($r));
+        $router->addRoute('GET', '/blog/manage', fn($r) => $blogController->manage($r));
+        $router->addRoute('GET', '/blog/create', fn($r) => $blogController->create($r));
+        $router->addRoute('POST', '/blog', fn($r) => $blogController->store($r));
+        $router->addRoute('GET', '/blog/(?<id>[0-9]+)/edit', fn($r) => $blogController->edit($r));
+        $router->addRoute('POST', '/blog/(?<id>[0-9]+)/edit', fn($r) => $blogController->update($r));
+        $router->addRoute('GET', '/blog/(?<id>[0-9]+)/delete', fn($r) => $blogController->deleteConfirm($r));
+        $router->addRoute('POST', '/blog/(?<id>[0-9]+)/delete', fn($r) => $blogController->delete($r));
+        $router->addRoute('GET', '/blog/(?<path>[a-z0-9-]+)', fn($r) => $blogController->show($r));
 
         $apiController = $container->get(ApiController::class);
-        $router->addRoute('GET','/api/grades',fn($r) => $apiController->grades($r));
-        $router->addRoute('GET','/api/grades/(?<id>[0-9]+)',fn($r) => $apiController->grade($r));
+        $router->addRoute('GET', '/api/grades', fn($r) => $apiController->grades($r));
+        $router->addRoute('GET', '/api/grades/(?<id>[0-9]+)', fn($r) => $apiController->grade($r));
     }
 }
